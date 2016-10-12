f807b3ec82391fcb29a195f27b47b62ea93a8f4c
Ivan Nikulin <zip753@gmail.com>
2016-10-12 23:19:19 +0200

    Update Validator documentation.

--- include/Validator.h
+++ include/Validator.h
@@ -7,8 +7,16 @@
 
 namespace SchedulingProblem {
 
+/** @brief Interface for validating schedules. */
 class Validator {
  public:
+  /**
+   * Checks whether schedule is valid considering resource assigment, task
+   * dependencies and skill requirements.
+   * @param s Schedule to validate.
+   * @return Pair of two elements: validity flag and output (possibly error)
+   * message.
+   */
   static std::pair<bool, std::string> validate(const Schedule& s);
 };
 
