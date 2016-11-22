6e4837275b5720b2f159ab5d31324762851a0c16
Ivan Nikulin <zip753@gmail.com>
2016-11-22 11:05:23 +0100

    Refactor validations.

--- src/Validator.cpp
+++ src/Validator.cpp
@@ -24,29 +24,21 @@ std::pair<bool, std::string> Validator::Validate(const Schedule &s) {
     // Check resource indices.
     if (s.capable_resource_idx(i) < 0 ||
         s.capable_resource_idx(i) >= s.task(i).num_capable_resources()) {
-      stream << "Resource index out of boundaries (task ID: ";
-      stream << s.task(i).id();
-      stream << ", resource index: ";
-      stream << s.capable_resource_idx(i);
-      stream << ", capable resources: ";
-      stream << s.task(i).num_capable_resources();
-      stream << ").";
-      return std::make_pair(false, stream.str());
+      stream << "Resource index out of boundaries (task ID: " << s.task(i).id()
+             << ", resource index: " << s.capable_resource_idx(i)
+             << ", capable resources: " << s.task(i).num_capable_resources()
+             << ").";
+      return {false, stream.str()};
     }
     // Check task dependencies.
     for (size_t j = 0; j < s.task(i).num_dependencies(); ++j) {
       size_t dep_idx = s.task(i).dependency(j);
       if (s.finish_time(dep_idx) >= s.start(i)) {
-        stream << "Task dependency not met (task #";
-        stream << s.task(i).id();
-        stream << " start time: ";
-        stream << s.start(i);
-        stream << ", task #";
-        stream << s.task(dep_idx).id();
-        stream << " finish time: ";
-        stream << s.finish_time(dep_idx);
-        stream << ").";
-        return std::make_pair(false, stream.str());
+        stream << "Task dependency not met (task #" << s.task(i).id()
+               << " start time: " << s.start(i)
+               << ", task #" << s.task(dep_idx).id()
+               << " finish time: " << s.finish_time(dep_idx) << ").";
+        return {false, stream.str()};
       }
     }
   }
@@ -61,30 +53,23 @@ std::pair<bool, std::string> Validator::Validate(const Schedule &s) {
   for (auto& res : res_tasks) {
     int res_id = res.first;
     std::vector<size_t>& tasks = res.second;
-    std::sort(tasks.begin(), tasks.end(), [&](size_t a, size_t b) {
-      return s.start(a) < s.start(b) ||
-          (s.start(a) == s.start(b) && s.finish_time(a) < s.finish_time(b));
+    std::sort(tasks.begin(), tasks.end(), [=](size_t a, size_t b) {
+      return {s.start(a), s.finish_time(a)} < {s.start(b), s.finish_time(b)};
     });
 
     for (size_t i = 0; i < tasks.size() - 1; ++i) {
       if (s.finish_time(tasks[i]) >= s.start(tasks[i + 1])) {
-        stream << "Resource #";
-        stream << res_id;
-        stream << " has conflicting assignments (task #";
-        stream << s.task(tasks[i]).id();
-        stream << " finish time: ";
-        stream << s.finish_time(tasks[i]);
-        stream << ", task #";
-        stream << s.task(tasks[i + 1]).id();
-        stream << " start time: ";
-        stream << s.start(tasks[i + 1]);
-        stream << ").";
-        return std::make_pair(false, stream.str());
+        stream << "Resource #" << res_id << " has conflicting assignments "
+               << "(task #" << s.task(tasks[i]).id()
+               << " finish time: " << s.finish_time(tasks[i])
+               << ", task #" << s.task(tasks[i + 1]).id()
+               << " start time: " << s.start(tasks[i + 1]) << ").";
+        return {false, stream.str()};
       }
     }
   }
 
-  return std::make_pair(true, "OK");
+  return {true, "OK"};
 }
 
 };  // namespace SchedulingProblem
